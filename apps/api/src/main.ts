baeb11da4729445867a414ffecb4f9cba87ee8a5
Robin Schellius <r.schellius@avans.nl>
2023-02-15 10:52:52 +0100

    Another commit for CI/CD

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,13 +5,17 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
+  const corsOptions: CorsOptions = {};
+  app.enableCors(corsOptions);
+
   const port = process.env.PORT || 3333;
   await app.listen(port);
   Logger.log(
